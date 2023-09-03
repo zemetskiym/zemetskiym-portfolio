12154bd07e0d3f8e4ff308c8f00881855af0c7ed
zemetskiym <zemetskiym@proton.me>
2023-09-02 19:21:01 -0700

    Feat: add Header.tsx with name and contact

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,20 @@
 import styles from "../styles/components/Header.module.css";
 import Globe from "./Globe";
+import Link from "next/link";
 
 export default function Header (): JSX.Element {
     return (
         <header id={styles.header}>
-            <Globe />
+            <h1 id={styles.title}>Matthew Zemetskiy</h1>
+            <div id={styles.globeContainer}>
+                <Globe />
+            </div>
+            <hr id={styles.hr} />
+            <ul id={styles.contacts}>
+                <li><Link href="mailto:zemetskiym@proton.me" target="_blank">zemetskiym@proton.me</Link></li>
+                <li><Link href="https://github.com/zemetskiym" target="_blank">github.com/zemetskiym</Link></li>
+                <li><Link href="https://www.linkedin.com/in/matthew-zemetskiy/" target="_blank">linkedin.com/in/matthew-zemetskiy</Link></li>
+            </ul>
         </header>
     );
 };
