67d142f070d0fe5cb5634f2998d9ae41f6182b94
zemetskiym <zemetskiym@proton.me>
2023-09-08 19:38:10 -0700

    Style: Breadcrumb add initial styles

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -1,10 +1,11 @@
-import Link from "next/link";
+import styles from '../styles/components/Breadcrumb.module.css';
+import Link from 'next/link';
 
 type Breadcrumb = {label: string, url: string | null};
 
 export default function Breadcrumb ({ breadcrumbs }: { breadcrumbs: Array<Breadcrumb> }): JSX.Element {
     return (
-        <nav>
+        <nav id={styles.breadcrumb}>
             {breadcrumbs.map((breadcrumb, index) => 
                 <span key={index}>
                     {breadcrumb.url ? (
@@ -12,7 +13,7 @@ export default function Breadcrumb ({ breadcrumbs }: { breadcrumbs: Array<Breadc
                     ) : (
                         <span>{breadcrumb.label}</span>
                     )}
-                    {index < breadcrumbs.length - 1 && <span>{' '}&gt;{' '}</span>}
+                    {index < breadcrumbs.length - 1 && <span>&nbsp;&gt;&nbsp;</span>}
                 </span>
             )}
         </nav>
