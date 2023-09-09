81f09961b794ce7207ba7c71f8d3360b1f7d8e4e
zemetskiym <zemetskiym@proton.me>
2023-09-08 20:40:44 -0700

    Style: ProjectList add initial styles

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -3,14 +3,14 @@ import projects from "../../public/json/projects.json";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Projects(): JSX.Element {
+export default function ProjectList(): JSX.Element {
     return (
-        <section>
-            <h1>Projects</h1>
-            <ul>
+        <section id={styles.projectList}>
+            <h1 id={styles.subheading}>Projects</h1>
+            <ul id={styles.imageList}>
                 {projects.map(project => 
-                    <Link href={`/projects/${project.id}`}>
-                    <Image src={project.images[0]} height={100} width={200} alt={project.title} className={styles.images} key={project.id} />
+                    <Link key={project.id} href={`/projects/${project.id}`}>
+                    <Image src={project.images[0]} sizes="242.5px" style={{width: '242.5px', height: 'auto'}} height={136.4} width={242.5} alt={project.title} className={styles.images} key={project.id} />
                     </Link>
                 )}
             </ul>
